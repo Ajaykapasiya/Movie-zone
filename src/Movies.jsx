6fcf2e95a4ade7140deea9c463676e558f996d65
ajay <ajaykumar290904@gmail.com>
2025-12-12 15:15:36 +0530

    feat: add movie filtering functionality and enhance layout in Movies component

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,28 +1,61 @@
 import React, { useState } from 'react'
-import {movies} from './data.js'
+import { movies } from './data.js'
 
 
 const Movies = () => {
 
     const [movieList, setMovieList] = useState(movies)
-    return (
-        <div style={{display:'flex',
-                    justifyContent:'center',
-                    alignContent:'center',
-                    flexWrap:'wrap',
-                    gap:'2rem'
-        }}>
-            {movieList.map((data) => (
-                <div key={data.id}>
-                    <div>
-                        <img src={data.poster_path} alt="" style={{width:'250px'}} />
-                    </div>
-                    <h3>{data.title}</h3>
-                    <p>{data.release_date}</p>
-                </div>))}
 
-        </div>
+    const filterBycategory = (cat) => {
+        setMovieList(movies.filter((data) => data.category == cat ))
+    }
+    return (
+        <>
+            <div className="my-3" style={{ width: "1000px", margin: "auto" }}>
+                <div className="mx-auto text-center">
+                    <button onClick={() => {setMovieList(movies)}} type="button" className="btn btn-outline-primary mx-3">All</button>
+                    <button onClick={() => {filterBycategory("Action")}}type="button" className="btn btn-outline-secondary mx-3">Action</button>
+                    <button onClick={() => {filterBycategory("Thriller")}}type="button" className="btn btn-outline-success mx-3">Thriller</button>
+                    <button onClick={() => {filterBycategory("Animation")}}type="button" className="btn btn-outline-danger mx-3">Animation</button>
+                    <button onClick={() => {filterBycategory("Horror")}}type="button" className="btn btn-outline-warning mx-3">Horror</button>
+                    <button onClick={() => {filterBycategory("Drama")}}type="button" className="btn btn-outline-info mx-3">Drama</button>
+                    <button onClick={() => {filterBycategory("Sci-Fi")}}type="button" className="btn btn-outline-light mx-3">Sci-fi</button>
+                </div>
+            </div>
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                flexWrap: 'wrap',
+                //gap:'2rem',
+                textAlign: 'center',
+                width: '1300px',
+                margin: 'auto',
+                marginTop: '1.5rem'
+            }}>
+                {movieList.map((data) => (
+                    <div key={data.id} style={{ maxWidth: "280px", textAlign: "center" }}>
+                        <div style={{ padding: "10px" }} className="hover_effect">
+                            <img
+                                src={data.poster_path}
+                                alt=""
+                                style={{
+                                    width: "200px",
+                                    height: "280px",
+                                    borderRadius: "10px",
+                                    border: "1px solid yellow",
+                                }}
+                            />
+                        </div>
+                        <h5>{data.title}</h5>
+                        <p>{data.release_date}</p>
+                    </div>))}
+
+            </div>
+        </>
     )
+
 }
 
 export default Movies
